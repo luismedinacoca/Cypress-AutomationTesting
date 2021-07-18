411aa7affcefd948d1b077ad593ccce51fc217f0
“luismedinacoca” <luismedinacoca@gmail.com>
2021-07-18 14:37:48 -0300

    Lecture041 - xpath selectors part3/3

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-Lecture 036 - Practical in Depth Look into Selectors  
-====================================================
-cypress/integration/webdriver/contact-us.js
-Feature: "contact-us"
-- in order to run just one scenario: 
-  * it.only()
-- create a new folder named automation-test-store
-  * create a contact-us.js fodler inside it
-- add the following code inside cypress/support/index.js
-  Cypress.Server.defaults({
-    whitelist: (xhr) => {
-      return true;
-    }
-  })
-  => issue:  xhr does not disappear
-- create a test for automation test store
+# Lecture 041 - Xpath Selectors
+
+cypress-io/cypress-xpath
+https://github.com/cypress-io/cypress-xpath
+
+Install npm dependencies for xpath:
+$ npm install -D cypress-xpath
+
+After running, add inside project/cypress/support/index.js, the following code:
+require(‘cypress-xpath’)
--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -2,11 +2,12 @@
 
 describe('Test Contact Us form via Automation Test Store', () => {
     it('Should be able to submit a successful submission via contact-us form', () => {
-        cy.visit('https://automationteststore.com/')
-        cy.get('.info_links_footer > :nth-child(5) > a').click()
-        cy.get('#ContactUsFrm_first_name').type('John Doe')
-        cy.get('#ContactUsFrm_email').type('John_doe@rgent.com')
-        cy.get('#ContactUsFrm_enquiry').type('Do you provide additional discount on bulk orders?')
-        cy.get('.col-md-6 > .btn').click()
+        cy.visit('https://automationteststore.com/');
+        //cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get('#ContactUsFrm_first_name').type('John Doe');
+        cy.get('#ContactUsFrm_email').type('John_doe@rgent.com');
+        cy.get('#ContactUsFrm_enquiry').type('Do you provide additional discount on bulk orders?');
+        cy.get('.col-md-6 > .btn').click();
     })
 })
